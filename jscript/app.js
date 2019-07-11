01a57e98683d09998a8c994bd782edde74752dfe
otarunero <44699301+otarunero@users.noreply.github.com>
2019-07-11 09:03:13 +0100

    updated

--- jscript/app.js
+++ jscript/app.js
@@ -1,24 +1,15 @@
-
-$(document).ready(function() {
+$(document).ready(function () {
+	var $body = $('body');
   
- 
-/* -----------------------------------------------
-                MOBILE NAVIGATION     
--------------------------------------------------- */
-
-  $(".burger-icon").on("click", function() {
-	  if($( ".main-nav" ).hasClass( "mobile-hide" )){
-		    $(".main-nav").removeClass("mobile-hide");
-		    $(".main-nav").addClass("mobile-show");
-	  }else{
-		      $(".main-nav").addClass("mobile-hide");
-		      $(".main-nav").removeClass("mobile-show");
-	  }
+	$body.on('click', 'div.master_list div.list-group button', function () {
+	  var $button = $(this),
+		article_option = $button.attr('data-option'),
+		article_selector = 'article.' + article_option,
+		$master_detail = $button.closest('.master_detail'),
+		$article = $master_detail.find(article_selector);
   
+	  $master_detail.find('article').removeClass('grow fadeIn');
+  
+	  $article.addClass('grow fadeIn');
+	});
   });
-    
-
-});
-
-
-
